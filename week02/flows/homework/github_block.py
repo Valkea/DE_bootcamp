2e93b8e4e5686ee570b2753534a95a3a1ca8c269
Emmanuel <github-sub@shedge.com>
2023-02-05 00:17:02 +0100

    expriment: try pushing to github

---
+++ week02/flows/homework/github_block.py
@@ -0,0 +1,8 @@
+from prefect.filesystems import GitHub
+
+block = GitHub(
+    repository="https://github.com/Valkea/DE_bootcamp.git",
+    # access_token=<my_access_token> # only required for private repos
+)
+block.get_directory("/week02/flows/homework") # specify a subfolder of repo
+block.save("ny-taxi-github")

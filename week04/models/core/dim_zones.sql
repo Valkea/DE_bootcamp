f19e1b842cfa7aef5d6a4144245909e4cec4010e
Emmanuel Letremble <dbt-sub@shedge.com>
2023-02-20 18:11:53 +0000

    Homework W4

--- week04/models/core/dim_zones.sql
+++ week04/models/core/dim_zones.sql
@@ -1,7 +1,7 @@
 {{ config(materialized='table') }}
 
 select
-  cast(LocationID as numeric) as locationid,
+  LocationID as locationid,
   Borough as borough,
   Zone as zone,
   replace(service_zone, 'Boro', 'Green') as service_zone -- because the CSV file contains "Boro" and "Yellow" zones...
